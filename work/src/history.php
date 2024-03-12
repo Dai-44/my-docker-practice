fd7072d119bb04a793947c6a907df272ef089031
Dai <kobayashi.daigo3@gmail.com>
2024-03-12 11:10:36 +0900

    feat: ネットワーク内でのコンテナ間通信のための設定を追記

--- work/src/history.php
+++ work/src/history.php
@@ -4,13 +4,13 @@
  * connect
  */
 
-$host = '???';
-$port = '???';
-$database = '???';
+$host = 'db';
+$port = '3306';
+$database = 'event';
 $dsn = sprintf('mysql:host=%s; port=%s; dbname=%s;', $host, $port, $database);
 
-$username = '???';
-$password = '???';
+$username = 'docker-handson-hoge';
+$password = 'docker-handson-password';
 
 $pdo = new PDO($dsn, $username, $password);
 
